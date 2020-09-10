69e246a575f4354dc8a0e69448c187f820fbefb6
Osvaldo <osvaldoleivabarria1@gmail.com>
2020-09-10 16:07:44 -0300

    METODOS PUT/POST/GET PARA STUDENT

--- src/main/java/com/example/primerAppSpringBoot/controller/StudentController.java
+++ src/main/java/com/example/primerAppSpringBoot/controller/StudentController.java
@@ -9,8 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping(value = "/")
@@ -22,4 +23,36 @@ public class StudentController {
     public ResponseEntity<Iterable<Student>> getAll(){
         return new ResponseEntity<Iterable<Student>>(student.findAll(), HttpStatus.OK);
     }
+    @PutMapping(value = "students")
+    public ResponseEntity<HttpStatus> newStudent(){
+        Student newStudent = new Student();
+        newStudent.setName("Felipe");
+        newStudent.setAge(19);
+        newStudent.setCourse("Java");
+        newStudent.setLastname("Leiva");
+        newStudent.setIdCourse(1);
+        student.save(newStudent);
+        return new ResponseEntity<HttpStatus>(HttpStatus.CREATED);
+    }
+    @PostMapping(value = "pathVariable/{id}")
+    public ResponseEntity<Student>findCourse(@PathVariable Integer id){
+        Student rsp = new Student();
+        rsp = student.findById(id);
+        return new ResponseEntity<Student>(rsp, HttpStatus.OK);
+    }
+    @PostMapping(value = "requestBody")
+    public ResponseEntity<Student>findCourse2(@RequestBody Student std){
+        Student rsp = new Student();
+        rsp = student.findById(std.getId());
+        return new ResponseEntity<Student>(rsp, HttpStatus.OK);
+    }
+    @PostMapping(value = "requestParam/")
+    public ResponseEntity<Student>findCourse2(@RequestParam Integer id){
+        Student rsp = new Student();
+        rsp = student.findById(id);
+        return new ResponseEntity<Student>(rsp, HttpStatus.OK);
+    }
+
+
+
 }
--- src/main/java/com/example/primerAppSpringBoot/repository/StudentRepository.java
+++ src/main/java/com/example/primerAppSpringBoot/repository/StudentRepository.java
@@ -4,4 +4,7 @@ import com.example.primerAppSpringBoot.entity.Student;
 import org.springframework.data.repository.CrudRepository;
 
 public interface StudentRepository extends CrudRepository<Student, Long> {
+
+    Student findById(Integer id);
+
 }
