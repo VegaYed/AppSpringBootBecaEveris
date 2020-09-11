976d78769ed65db6875d1d2ca32ff77defe597ba
Osvaldo <osvaldoleivabarria1@gmail.com>
2020-09-11 00:02:48 -0300

    Ocupando PostMapping y request body implemento un metodo para recibir un course el metodo retorna 200 si el course existe y si no 404 esto se implemento ocupando Java Optional, se comentaron metodos de prueba.

--- src/main/java/com/example/primerAppSpringBoot/controller/CourseController.java
+++ src/main/java/com/example/primerAppSpringBoot/controller/CourseController.java
@@ -6,20 +6,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping(value = "/")
 public class CourseController {
 
     @Autowired
-    CourseRepository course;
+    CourseRepository repCourse;
 
     @GetMapping(value = "courses")
     public ResponseEntity<Iterable<Course>>getAll(){
-        return new ResponseEntity<Iterable<Course>>(course.findAll(), HttpStatus.OK);
+        return new ResponseEntity<Iterable<Course>>(repCourse.findAll(), HttpStatus.OK);
     }
-
-
+    /*@GetMapping(value = "course/{id}")
+    public ResponseEntity<Course>getCourseByPath(@PathVariable Integer id){
+        Course course = repCourse.findById(id);
+        return new ResponseEntity<Course>(course,HttpStatus.OK);
+    }
+    @PostMapping(value = "course/")
+    public ResponseEntity<Course>getCourseByRequest(@RequestParam Integer id) {
+        Course course = repCourse.findById(id);
+        return new ResponseEntity<Course>(course, HttpStatus.OK);
+    }*/
+    @PostMapping(value = "courseB")
+    public ResponseEntity<Course>getCourseByBody(@RequestBody Course courseRetrived) {
+        Optional<Course> course = repCourse.findById(courseRetrived.getId());
+        return course.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 }
